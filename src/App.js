9f4a50843186060fba0d3ec02a1f3b44fe66c9ba
SabuShakya <sabushakya1@gmail.com>
2020-08-20 08:59:09 +0545

    Error handling and validation

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react"
 import { BrowserRouter as Router } from "react-router-dom"
 import Routes from "routes"
 import Topbar from "components/topbar"
+import { CurrentUserProvider } from "contexts/currentUser"
 
 function App() {
   return (
-    <Router>
-      <Topbar />
-      <Routes />
-    </Router>
+    <CurrentUserProvider>
+      <Router>
+        <Topbar />
+        <Routes />
+      </Router>
+    </CurrentUserProvider>
   )
 }
 
